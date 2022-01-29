cc257e5fc0de52d84b332bc26e5f2e16ead4f5d9
Brian <beavelar.github@gmail.com>
2022-01-29 15:43:19 -0700

    Update environment and tests

--- src/node/util/env/environment.js
+++ src/node/util/env/environment.js
@@ -15,69 +15,79 @@ class Environment {
   _logger = new Logger('env');
 
   constructor(options) {
-    this._parseOptions(options);
+    this.values = this._parseOptions(options);
+    if (this.values.size === 0) {
+      this.validKeys = false;
+    }
   }
 
+  /**
+   * With the provided options, will attempt to retrieve the each desired environment
+   * variable from the process environment and parse to the desired option type.
+   * 
+   * @param {Object<string, string>} options The provided options to attempt to parse
+   * and to parse to. The key should be the environment variable to attemp to parse
+   * and the value should be the type to attempt to parse to.
+   * @returns A map containing the result. If any of the options are unable to be
+   * parsed, the map will be empty.
+   */
   _parseOptions(options) {
+    const result = new Map();
     for (const key in options) {
       this._logger.debug('parseOptions', `parsing environment option ${key} of type ${options[key]}`);
-      if (process.env[key] === undefined) {
-        this._logger.critical('parseOptions', `${key} does not exist as a environment variable`);
-        this.validKeys = false;
-        break;
-      }
+      const parsedOption = this._parseOption(key, options[key]);
+      result.set(key, parsedOption);
 
-      const validOption = this._parseOption(key, options[key]);
-      if (!validOption) {
-        this._logger.critical('parseOptions', 'unable to parse options provided');
-        this.validKeys = false;
-        break;
+      if (parsedOption === undefined) {
+        this._logger.critical('parseOptions', `unable to parse provided options`);
+        return new Map();
       }
     }
+    return result;
   }
 
+  /**
+   * With the provided option and optionType, will try to retrieve the environment
+   * variable from the process environment and parse to the desired variable type.
+   * 
+   * @param {string} option The environment variable to attempt to parse 
+   * @param {string} optionType What to attempt to parse the environment variable to.
+   * Valid options are 'float', 'int', and 'string'
+   * @returns The parsed option to the desired type or undefined if invalid
+   */
   _parseOption(option, optionType) {
-    switch (optionType) {
-      case 'float':
-        try {
-          const result = parseFloat(process.env[option]);
-          if (isNaN(result)) {
-            this._logger.critical('parseOption', `unable to cast value of ${option} - ${process.env[option]} to float`);
-            return false;
-          }
-          else {
-            this.values.set(option, result);
-          }
-        }
-        catch {
-          this._logger.critical('parseOption', `unable to cast value of ${option} - ${process.env[option]} to float`);
-          return false;
-        }
-        break;
-      case 'int':
-        try {
-          const result = parseInt(process.env[option]);
-          if (isNaN(result)) {
-            this._logger.critical('parseOption', `unable to cast value of ${option} - ${process.env[option]} to int`);
-            return false;
-          }
-          else {
-            this.values.set(option, result);
-          }
-        }
-        catch {
-          this._logger.critical('parseOption', `unable to cast value of ${option} - ${process.env[option]} to int`);
-          return false;
-        }
-        break;
-      case 'string':
-        this.values.set(option, process.env[option]);
-        break;
-      default:
-        this._logger.critical('parseOption', `unknown option type provided - ${optionType}`);
-        return false;
+    let result = undefined;
+    if (process.env[option] === undefined || process.env[option] === null) {
+      this._logger.critical('parseOption', `${option} does not exist as a environment variable`);
+      result = undefined;
     }
-    return true;
+    else if (process.env[option] === '') {
+      this._logger.critical('parseOption', `no value for environment variable ${option} was provided`);
+      result = undefined;
+    }
+    else {
+      if (optionType === 'float') {
+        result = parseFloat(process.env[option]);
+        if (isNaN(result)) {
+          this._logger.critical('parseOption', `unable to cast value of ${option} - ${process.env[option]} to float`);
+          result = undefined;
+        }
+      }
+      else if (optionType === 'int') {
+        result = parseInt(process.env[option]);
+        if (isNaN(result)) {
+          this._logger.critical('parseOption', `unable to cast value of ${option} - ${process.env[option]} to int`);
+          result = undefined;
+        }
+      }
+      else if (optionType === 'string') {
+        result = process.env[option];
+      }
+      else {
+        this._logger.critical('parseOption', `unknown option type provided - ${optionType}`);
+      }
+    }
+    return result;
   }
 }
 
--- src/node/util/env/environment.test.js
+++ src/node/util/env/environment.test.js
@@ -9,7 +9,7 @@ beforeEach(() => {
 test('test Environment no options', () => {
   const env = new Environment({});
   expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
+  expect(env.validKeys).toEqual(false);
 });
 
 test('test Environment missing env', () => {
@@ -53,134 +53,103 @@ test('test Environment invalid env', () => {
 
 test('test _parseOptions no options', () => {
   const env = new Environment({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
+  const parsedOptions = env._parseOptions({});
 
-  env._parseOptions({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
+  expect(parsedOptions.size).toEqual(0);
 });
 
 test('test _parseOptions missing env', () => {
   const env = new Environment({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
-
-  env._parseOptions({
+  const parsedOptions = env._parseOptions({
     "INT_VAR": "int",
     "FLOAT_VAR": "float",
     "STRING_VAR": "string"
   });
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(false);
+
+  expect(parsedOptions.size).toEqual(0);
 });
 
 test('test _parseOptions valid env', () => {
-  const env = new Environment({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
-
   process.env.INT_VAR = '1';
   process.env.FLOAT_VAR = '1.2';
   process.env.STRING_VAR = 'env-value';
-  env._parseOptions({
+
+  const env = new Environment({});
+  const parsedOptions = env._parseOptions({
     "INT_VAR": "int",
     "FLOAT_VAR": "float",
     "STRING_VAR": "string"
   });
-  expect(env.values.size).toEqual(3);
-  expect(env.values.get('INT_VAR')).toEqual(1);
-  expect(env.values.get('FLOAT_VAR')).toEqual(1.2);
-  expect(env.values.get('STRING_VAR')).toEqual('env-value');
-  expect(env.validKeys).toEqual(true);
+
+  expect(parsedOptions.size).toEqual(3);
+  expect(parsedOptions.get('INT_VAR')).toEqual(1);
+  expect(parsedOptions.get('FLOAT_VAR')).toEqual(1.2);
+  expect(parsedOptions.get('STRING_VAR')).toEqual('env-value');
 });
 
 test('test _parseOptions invalid env', () => {
-  const env = new Environment({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
-
   process.env.INT_VAR = 'string-value-1';
   process.env.FLOAT_VAR = 'string-value-2';
-  env._parseOptions({
+
+  const env = new Environment({});
+  const parsedOptions = env._parseOptions({
     "INT_VAR": "int",
     "FLOAT_VAR": "float"
   });
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(false);
+
+  expect(parsedOptions.size).toEqual(0);
 });
 
 test('test _parseOption valid float', () => {
-  const env = new Environment({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
-
   process.env.FLOAT_VAR = '1.2';
-  const valid = env._parseOption('FLOAT_VAR', 'float');
 
-  expect(valid).toEqual(true);
-  expect(env.values.size).toEqual(1);
-  expect(env.values.get('FLOAT_VAR')).toEqual(1.2);
+  const env = new Environment({});
+  const parsedOption = env._parseOption('FLOAT_VAR', 'float');
+
+  expect(parsedOption).toEqual(1.2);
 });
 
 test('test _parseOption invalid float', () => {
-  const env = new Environment({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
-
   process.env.FLOAT_VAR = 'string-value';
-  const valid = env._parseOption('FLOAT_VAR', 'float');
 
-  expect(valid).toEqual(false);
-  expect(env.values.size).toEqual(0);
+  const env = new Environment({});
+  const parsedOption = env._parseOption('FLOAT_VAR', 'float');
+
+  expect(parsedOption).toBeUndefined();
 });
 
 test('test _parseOption valid int', () => {
-  const env = new Environment({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
-
   process.env.INT_VAR = '1';
-  const valid = env._parseOption('INT_VAR', 'int');
 
-  expect(valid).toEqual(true);
-  expect(env.values.size).toEqual(1);
-  expect(env.values.get('INT_VAR')).toEqual(1);
+  const env = new Environment({});
+  const parsedOption = env._parseOption('INT_VAR', 'int');
+
+  expect(parsedOption).toEqual(1);
 });
 
 test('test _parseOption invalid int', () => {
-  const env = new Environment({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
-
   process.env.INT_VAR = 'string-value';
-  const valid = env._parseOption('INT_VAR', 'int');
 
-  expect(valid).toEqual(false);
-  expect(env.values.size).toEqual(0);
+  const env = new Environment({});
+  const parsedOption = env._parseOption('INT_VAR', 'int');
+
+  expect(parsedOption).toBeUndefined();
 });
 
 test('test _parseOption string', () => {
-  const env = new Environment({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
-
   process.env.STRING_VAR = 'string-value';
-  const valid = env._parseOption('STRING_VAR', 'string');
 
-  expect(valid).toEqual(true);
-  expect(env.values.size).toEqual(1);
-  expect(env.values.get('STRING_VAR')).toEqual('string-value');
+  const env = new Environment({});
+  const parsedOption = env._parseOption('STRING_VAR', 'string');
+
+  expect(parsedOption).toEqual('string-value');
 });
 
 test('test _parseOption unknown option type', () => {
-  const env = new Environment({});
-  expect(env.values.size).toEqual(0);
-  expect(env.validKeys).toEqual(true);
-
   process.env.STRING_VAR = 'string-value';
-  const valid = env._parseOption('STRING_VAR', 'custom-type');
 
-  expect(valid).toEqual(false);
-  expect(env.values.size).toEqual(0);
+  const env = new Environment({});
+  const parsedOption = env._parseOption('STRING_VAR', 'custom-type');
+
+  expect(parsedOption).toBeUndefined();
 });
