e9d14bca65fe6c9d0014249034e8080814392a9f
Brian <beavelar.github@gmail.com>
2022-01-29 17:36:00 -0700

    Send basic help menu when help command is received

--- src/node/discord/discord.js
+++ src/node/discord/discord.js
@@ -1,16 +1,31 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Message, Channel } = require('discord.js');
 const { Logger } = require('../util/logger/logger.js');
 
+/**
+ * Basic wrapper of the Discord bot client implementation
+ */
 class Discord {
-  /** Logger for Discord */
-  _logger = new Logger('discord');
-
   /** The Discord bot client */
   _client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
     partials: ['MESSAGE', 'CHANNEL']
   });
 
+  /** The message to display whenever a help command is received */
+  _helpMessage =
+    '\`\`\`\n' +
+    '------------------------------------------------------------\n' +
+    'Help Menu\n' +
+    '------------------------------------------------------------\n' +
+    'No available commands at the moment, sorry!\n' +
+    '\`\`\`'
+
+  /** Logger for Discord */
+  _logger = new Logger('discord');
+
+  /**
+   * @param {string} token The string token to utilize to login the Discord bot
+   */
   constructor(token) {
     if (token) {
       // Setup handlers
@@ -18,6 +33,7 @@ class Discord {
       this._client.on('messageCreate', this._messageCreate.bind(this));
 
       // Login discord bot utilizing the bot token
+      this._logger.info('Discord', 'logging in as Discord bot utilizing token');
       this._client.login(token);
     }
     else {
@@ -37,19 +53,36 @@ class Discord {
    * Client messageCreate handler. We will handle the message here.
    * 
    * @param {Message} message The message caught by the messageCreate event
-   * @returns 
    */
   _messageCreate(message) {
     if (message.author.bot) return;
-    this._logger.debug('onMessageCreate', 'received message event');
+    this._logger.debug('messageCreate', 'received messageCreate event');
 
-    if (message.content.startsWith('!hello')) {
-      message.channel.send('Hello World :)').then((message) => {
-        this._logger.debug('onMessage', `successfully sent message to ${message.guild.id}`);
-      }).catch((error) => {
-        this._logger.critical('onMessage', 'error occurred attempting to reply to command', error);
-      });
+    if (message.content.startsWith('!pt')) {
+      this._logger.debug('messageCreate', 'received !pt trigger');
+      const command = message.content.replace('!pt', '').trim();
+      if (command.includes('help')) {
+        this._logger.info('messageCreate', 'received help command');
+        this._sendMessage(message.channel, this._helpMessage);
+      }
+      else {
+        this._logger.info('messageCreate', `unknown command received: ${command}`);
+      }
     }
+  }
+
+  /**
+   * Helper function to send a desired message to a desired channel
+   * 
+   * @param {Channel} channel The discord channel to send the message to
+   * @param {string} message The desired message to send
+   */
+  _sendMessage(channel, message) {
+    channel.send(message).then((response) => {
+      this._logger.info('sendMessage', `successfully sent message to ${response.guild.id}`);
+    }).catch((error) => {
+      this._logger.critical('sendMessage', 'error occurred attempting to send message', error);
+    });
   }
 }
 
