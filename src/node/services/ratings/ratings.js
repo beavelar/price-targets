bcc67f162042d30fe22a359ab1bd287a371c7166
Brian <beavelar.github@gmail.com>
2022-01-31 15:37:03 -0700

    Include usage of Environment

--- src/node/services/ratings/ratings.js
+++ src/node/services/ratings/ratings.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const { Environment } = require('../../util/env/environment.js');
 const { Logger } = require('../../util/logger/logger.js');
 
-const server = express();
-const logger = new Logger('ratings');
+const env = new Environment({ 'RATINGS_SERVER_PORT': 'int' });
+if (env.validKeys) {
+  const server = express();
+  const logger = new Logger('ratings');
 
-server.get('/ratings', (req, res) => {
-  logger.debug('GET', 'received GET request');
-  res.send('hello :)');
-});
+  server.get('/ratings', (req, res) => {
+    logger.debug('GET', 'received GET request');
+    res.send('hello :)');
+  });
 
-server.listen('8090', () => {
-  logger.info('init', `server is up and listening on port: 8090`);
-});
+  server.listen(`${env.values.get('RATINGS_SERVER_PORT')}`, () => {
+    logger.info('init', `server is up and listening on port: ${env.values.get('RATINGS_SERVER_PORT')}`);
+  });
+}
