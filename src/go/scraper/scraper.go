bb168dffdffefac6d4945bfe8c457820e54fccc7
Brian <beavelar.github@gmail.com>
2022-01-30 19:43:28 -0700

    Add basic logging

--- src/go/scraper/scraper.go
+++ src/go/scraper/scraper.go
@@ -3,15 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/op/go-logging"
 )
 
+var logger = logging.MustGetLogger("scraper")
+
+// ratings route implementation
 func ratings(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("received /ratings request")
+	if req.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	logger.Debug("received /ratings request")
+	fmt.Fprintf(w, "hello :)")
 }
 
 func main() {
+	// Setup logger
+	format := logging.MustStringFormatter(`%{level}: %{shortfile}.%{shortfunc} - %{message}`)
+	logging.SetFormatter(format)
+	logging.SetLevel(logging.DEBUG, "scraper")
+
+	// Setup route
 	http.HandleFunc("/ratings", ratings)
 
-	fmt.Println("server is up and listening on port 8090")
-	http.ListenAndServe(":8090", nil)
+	// Setup listen and serve
+	logger.Info("starting server on port 8090")
+	if err := http.ListenAndServe(":8090", nil); err != nil {
+		logger.Critical(err)
+	}
 }
