6c5887ac24de545170f05eb83c0e5734381bb0f7
Brian <beavelar.github@gmail.com>
2022-01-31 15:16:18 -0700

    Directory restructure

--- src/node/services/bot/bot.js
+++ src/node/services/bot/bot.js
@@ -1,6 +1,6 @@
-const { Discord } = require('../discord/discord.js');
-const { Logger } = require('../util/logger/logger.js');
-const { Environment } = require('../util/env/environment.js');
+const { Discord } = require('../../util/discord/discord.js');
+const { Logger } = require('../../util/logger/logger.js');
+const { Environment } = require('../../util/env/environment.js');
 
 const logger = new Logger('bot');
 const env = new Environment({ DISCORD_BOT_TOKEN: 'string' });
