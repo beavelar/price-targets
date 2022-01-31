190f3efb733fe57752c72e3eadb8189e7e1612af
Brian <beavelar.github@gmail.com>
2022-01-31 16:10:47 -0700

    Request data and respond with analyst ratings

--- src/node/services/bot/bot.js
+++ src/node/services/bot/bot.js
@@ -1,6 +1,6 @@
 const { Discord } = require('../../util/discord/discord.js');
-const { Logger } = require('../../util/logger/logger.js');
 const { Environment } = require('../../util/env/environment.js');
+const { Logger } = require('../../util/logger/logger.js');
 
 const logger = new Logger('bot');
 const env = new Environment({ DISCORD_BOT_TOKEN: 'string' });
--- src/node/services/ratings/ratings.js
+++ src/node/services/ratings/ratings.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const needle = require('needle');
 const { Environment } = require('../../util/env/environment.js');
 const { Logger } = require('../../util/logger/logger.js');
 
-const env = new Environment({ 'RATINGS_SERVER_PORT': 'int' });
+const env = new Environment({
+  'RATINGS_SERVER_PORT': 'int',
+  'RATINGS_REQUEST_URL': 'string'
+});
+
 if (env.validKeys) {
   const server = express();
   const logger = new Logger('ratings');
 
-  server.get('/ratings', (req, res) => {
-    logger.debug('GET', 'received GET request');
-    res.send('hello :)');
+  server.get('/ratings/:ticker', (req, res) => {
+    const ticker = req.params.ticker.toLocaleLowerCase();
+    logger.debug('GET', `received GET request for ticker ${ticker}`);
+    needle.get(`${env.values.get('RATINGS_REQUEST_URL')}/${ticker}/payload.json`, (err, _res) => {
+      if (err) {
+        logger.critical('GET', 'error occurred attempting to retrieve ratings', err);
+        res.status(500).send({ error: `error occurred attempting to retrieve ratings ${err}` });
+      }
+      else {
+        logger.debug('GET', 'successfully retrieved ratings data');
+        if (_res.body.analysts && _res.body.analysts.ratings) {
+          logger.debug('GET', 'responding with analyst ratings data');
+          res.status(200).send({ data: _res.body.analysts.ratings });
+        }
+      }
+    });
   });
 
   server.listen(`${env.values.get('RATINGS_SERVER_PORT')}`, () => {
