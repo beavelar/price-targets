987c29fbcc144047f8ccb096ba157cb96a2c20ca
Brian <beavelar.github@gmail.com>
2022-02-01 18:21:58 -0700

    Update help message

--- src/node/util/discord/discord.js
+++ src/node/util/discord/discord.js
@@ -1,4 +1,10 @@
-const { Client, Intents, Message, Channel } = require('discord.js');
+const {
+  Channel,
+  Client,
+  Intents,
+  Message,
+  MessageEmbed
+} = require('discord.js');
 const { Logger } = require('../logger/logger.js');
 
 /**
@@ -11,14 +17,27 @@ class Discord {
     partials: ['MESSAGE', 'CHANNEL']
   });
 
+  /** The description of the help message to display whenever a help command is received */
+  _helpDescriptionMessage = 'Price Targets bot can be utilized to view price target ' +
+    'updates and current price targets provided by various analysts. Price target ' +
+    'updates will get displayed at 7AM MST Monday - Friday';
+
   /** The message to display whenever a help command is received */
-  _helpMessage =
-    '\`\`\`\n' +
-    '------------------------------------------------------------\n' +
-    'Help Menu\n' +
-    '------------------------------------------------------------\n' +
-    'No available commands at the moment, sorry!\n' +
-    '\`\`\`'
+  _helpMessage = new MessageEmbed({
+    title: 'Help Menu',
+    color: '#00D100',
+    description: this._helpDescriptionMessage,
+    fields: [{
+      name: 'Commands',
+      value: '`pt!rating`, `pt!help`'
+    }, {
+      name: '__pt!rating__',
+      value: 'Request the current lowest, highest, and average price targets provided by various analysts'
+    }, {
+      name: '__pt!help__',
+      value: 'Display the help menu to view all available commands'
+    }]
+  });
 
   /** Logger for Discord */
   _logger = new Logger('discord');
@@ -58,12 +77,15 @@ class Discord {
     if (message.author.bot) return;
     this._logger.debug('messageCreate', 'received messageCreate event');
 
-    if (message.content.startsWith('!pt')) {
-      this._logger.debug('messageCreate', 'received !pt trigger');
-      const command = message.content.replace('!pt', '').trim();
-      if (command.includes('help')) {
+    if (message.content.startsWith('pt!')) {
+      this._logger.debug('messageCreate', 'received pt! trigger');
+      const command = message.content.replace('pt!', '').trim();
+      if (command.startsWith('help')) {
         this._logger.info('messageCreate', 'received help command');
         this._sendMessage(message.channel, this._helpMessage);
+      }
+      else if (command.startsWith('rating')) {
+        this._logger.info('messageCreate', 'received rating command');
       }
       else {
         this._logger.warning('messageCreate', `unknown command received: ${command}`);
@@ -75,10 +97,11 @@ class Discord {
    * Helper function to send a desired message to a desired channel
    * 
    * @param {Channel} channel The discord channel to send the message to
-   * @param {string} message The desired message to send
+   * @param {string | MessageEmbed} message The desired message to send
    */
   _sendMessage(channel, message) {
-    channel.send(message).then((response) => {
+    const msg = typeof message === 'string' ? message : { embeds: [message] };
+    channel.send(msg).then((response) => {
       this._logger.info('sendMessage', `successfully sent message to ${response.guild.id}`);
     }).catch((error) => {
       this._logger.critical('sendMessage', 'error occurred attempting to send message', error);
