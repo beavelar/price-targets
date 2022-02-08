39e2ac3422f4afce11eec642dfc894250efd599c
Brian <beavelar.github@gmail.com>
2022-02-07 21:45:02 -0700

    Request data from ticker service before requesting analyst data

--- src/node/services/ratings/main.js
+++ src/node/services/ratings/main.js
@@ -4,13 +4,14 @@ const { RatingsServer } = require('./server/server.js');
 
 const env = new Environment({
   'RATINGS_REQUEST_URL': 'string',
-  'RATINGS_SERVER_PORT': 'int'
+  'RATINGS_SERVER_PORT': 'int',
+  'TICKER_SERVICE_ENDPOINT': 'string'
 });
 
 const logger = new Logger('ratings');
 
 if (env.validKeys) {
-  const server = new RatingsServer(env.get('RATINGS_REQUEST_URL'));
+  const server = new RatingsServer(env.get('RATINGS_REQUEST_URL'), env.get('TICKER_SERVICE_ENDPOINT'));
   server.init(env.get('RATINGS_SERVER_PORT'))
 }
 else {
--- src/node/services/ratings/server/server.js
+++ src/node/services/ratings/server/server.js
@@ -6,16 +6,25 @@ const { Logger } = require('../../../util/logger/logger.js');
  * Wrapper for the rating server
  */
 class RatingsServer {
+  /** The URL of the data provider */
+  _dataUrl = undefined;
+
   /** Logger for RatingsServer */
   _logger = new Logger('RatingsServer');
 
   /** The express server instance */
   _server = express();
 
-  constructor(dataUrl) {
+  /** The URL of the ticker service */
+  _tickerUrl = undefined;
+
+  constructor(dataUrl, tickerUrl) {
+    this._dataUrl = dataUrl;
+    this._tickerUrl = tickerUrl;
+
     /* istanbul ignore next */
-    if (dataUrl) {
-      this._ratingsRoute(dataUrl);
+    if (this._dataUrl && this._tickerUrl) {
+      this._ratingsRoute();
     }
   }
 
@@ -32,11 +41,38 @@ class RatingsServer {
   }
 
   /**
+   * Helper function to request data from the analyst data api.
+   * 
+   * @param {string} ticker The ticker to request data for from the ticker service
+   * @returns {Promise<{error: string} | {average: number, highest: number, lowest: number}>} A promise
+   * which will resolve to the parse data from the data retrieved or reject with a message
+   */
+  /* istanbul ignore next */
+  _dataRequest(ticker) {
+    return new Promise((resolve, reject) => {
+      needle.get(`${this._dataUrl}/${ticker.toLowerCase()}/payload.json`, (err, res) => {
+        if (err) {
+          this._logger.critical('dataRequest', 'error occurred attempting to retrieve ratings', err);
+          reject({ error: 'error occurred attempting to retrieve ratings', trace: err });
+        }
+        else {
+          this._logger.debug('dataRequest', `successfully retrieved ratings data for ${ticker}`);
+          if (res.body.analysts && res.body.analysts.ratings) {
+            resolve(this._getLowHighAverage(res.body.analysts.ratings));
+          }
+          resolve({ average: 0, highest: 0, lowest: 0 });
+        }
+      });
+    });
+  }
+
+  /**
    * Helper function to parse through the ratings and retrieve the highest and lowest rating,
    * as well as the average value of all the provided ratings.
    * 
    * @param {[any]} ratings The provided ratings to parse
-   * @returns An object containing the highest, lowest, and average value of ratings
+   * @returns {{average: number, highest: number, lowest: number}} An object containing the highest,
+   * lowest, and average value of ratings
    */
   _getLowHighAverage(ratings) {
     this._logger.debug('getLowHighAverage', 'retrieving low, high, and average value of the ratings');
@@ -72,25 +108,52 @@ class RatingsServer {
   /**
    * Creates the /ratings/:ticker route and add the logic to run whenever the
    * route is reached.
-   * 
-   * @param {string} dataUrl The url to request the data from
    */
   /* istanbul ignore next */
-  _ratingsRoute(dataUrl) {
+  _ratingsRoute() {
     this._server.get('/ratings/:ticker', (req, res) => {
-      const ticker = req.params.ticker.toLowerCase();
-      this._logger.debug('GET', `received GET request for ticker ${ticker.toUpperCase()}`);
-      needle.get(`${dataUrl}/${ticker}/payload.json`, (err, _res) => {
+      const ticker = req.params.ticker.toUpperCase();
+      this._logger.debug('GET', `received GET request for ticker ${ticker}`);
+
+      this._tickerRequest(ticker).then((tickerData) => {
+        this._dataRequest(ticker.toLowerCase()).then((ratingData) => {
+          this._logger.debug('GET', `responding with analyst ratings data for ${ticker}`);
+          res.status(200).send({ info: tickerData, rating: ratingData });
+        }).catch((dataErr) => {
+          this._logger.warning('GET', dataErr.error, dataErr.trace);
+          res.status(500).send(tickerErr);
+        });
+      }).catch((tickerErr) => {
+        this._logger.warning('GET', tickerErr.error, tickerErr.trace);
+        res.status(500).send(tickerErr);
+      });
+    });
+  }
+
+  /**
+   * Helper function to request data from the ticker service.
+   * 
+   * @param {string} ticker The ticker to request data for from the ticker service
+   * @returns {Promise<{error: string} | {companyName: string, price: number, symbol: string}>} A promise which
+   * will resolve/reject to the response/error from the ticker service
+   */
+  /* istanbul ignore next */
+  _tickerRequest(ticker) {
+    return new Promise((resolve, reject) => {
+      needle.get(`${this._tickerUrl}?symbol=${ticker}`, (err, res) => {
         if (err) {
-          this._logger.critical('GET', 'error occurred attempting to retrieve ratings', err);
-          res.status(500).send({ error: `error occurred attempting to retrieve ratings ${err}` });
+          this._logger.critical('tickerRequest', 'error occurred attempting to reach ticker service', err);
+          reject({ error: 'error occurred attempting to reach ticker service', trace: err });
         }
         else {
-          this._logger.debug('GET', 'successfully retrieved ratings data');
-          if (_res.body.analysts && _res.body.analysts.ratings) {
-            const rating = this._getLowHighAverage(_res.body.analysts.ratings);
-            this._logger.debug('GET', 'responding with analyst ratings data');
-            res.status(200).send({ data: rating });
+          const response = JSON.parse(res.body);
+          if (response.error) {
+            this._logger.warning('tickerRequest', `invalid ticker provided (${ticker}), unable to retrieve data`, response.error);
+            reject({ error: `invalid ticker provided (${ticker}), unable to retrieve data`, trace: response.error });
+          }
+          else {
+            this._logger.debug('tickerRequest', `successfully received ticker data for ${ticker}`);
+            resolve(response);
           }
         }
       });
